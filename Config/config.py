bcdf50dc2a493a2d41d7a35e2beb35d6d10e3c84
JohanCamiloMesa <jcmesar@academia.usbbog.edu.co>
2025-09-17 10:07:26 -0500

    Development: Code structure

---
+++ Config/config.py
@@ -0,0 +1,6 @@
+class Config:
+    """
+    Clase de configuración para rutas y parámetros del ETL.
+    """
+    INPUT_PATH = 'D:\\ETLPremier\\Extract\\Files\\England CSV.csv'
+    OUTPUT_PATH = 'D:\\ETLPremier\\Extract\\Files\\output_clean.csv'
